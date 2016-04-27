47002ef66988bc3e2ef0d9841b66f7d989ab5e01
Marcos <marcosvenezuela.araujo@gmail.com>
2016-04-27 10:51:35 -0300

    Novos items

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,11 @@
 // public/js/main.js
 
 angular.module('artebalaoweb', ['ngRoute', 'ngMaterial'])
-    .config(function ($routeProvider) {});
+    .config(function ($routeProvider) {
+        
+        $routeProvider.when('/portfolio', {
+           templateUrl: 'pages/portfolio/index.html',
+           Controller: 'PortfolioController.js' 
+        });
+        
+    });
